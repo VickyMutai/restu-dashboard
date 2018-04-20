9f9bd4d70d9d4475e36a30eb837b1e60a1f15634
VickyMutai <vicky.mutai96@gmail.com>
2018-04-20 19:58:40 +0300

    checking if everything works

--- rest/settings.py
+++ rest/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'dashb.apps.DashbConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
